9f4566e07d415808ac32cd9dc2e6002c5d7a7da8
Bruno.Diniz <diniz.bd@gmail.com>
2010-02-13 12:39:18 -0200

    configure environment

--- config/environment.rb
+++ config/environment.rb
@@ -18,13 +18,8 @@ Rails::Initializer.run do |config|
   # config.gem "bj"
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
-<<<<<<< HEAD:config/environment.rb
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "inherited_resources", :version => "1.0.2"
-=======
-  # config.gem "aws-s3", :lib => "aws/s3"
-  config.gem "josevalim-inherited_resources", :lib => "inherited_resources", :source => "http://gems.github.com"
->>>>>>> 8677e7e5144dda4cba8207adfb3f00dc1bc290e7:config/environment.rb
   config.gem "factory_girl", :source => "http://gemcutter.org"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
